dc0505c6d620171713c76f92858e806c03e1ef72
justinrasler <85715506+justinrasler@users.noreply.github.com>
2022-07-29 13:02:29 -0400

    other solutions

--- practice.js
+++ practice.js
@@ -3,3 +3,22 @@
 function smash (words) {
     return words.join(" ")
  };
+//7 kyu problems
+  ////////////////////////////////////////////////////////
+// is it even
+function testEven(n) {
+  if(n%2 === 0){
+    return true
+  } else {
+    return false
+  }
+}
+// other solutions
+function testEven(n){
+    return !(n % 2);
+  }
+
+  function testEven(n) {
+    return n % 2 === 0 ? true : false;
+ }
+  ////////////////////////////////////////////////////////
