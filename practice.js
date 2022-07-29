76f3bc565f642ce3363b21e29c72a6a1b21c4a4b
justinrasler <85715506+justinrasler@users.noreply.github.com>
2022-07-29 13:39:54 -0400

    personalized message

--- practice.js
+++ practice.js
@@ -1,11 +1,14 @@
 //8 kyu problem
-// sentence smash
+// sentence smash (return array of words joined as a sentence)
 function smash (words) {
     return words.join(" ")
  };
+   ////////////////////////////////////////////////////////
+     ////////////////////////////////////////////////////////
 //7 kyu problems
   ////////////////////////////////////////////////////////
-// is it even
+    ////////////////////////////////////////////////////////
+// is it even (if even return true, if odd return false)
 function testEven(n) {
   if(n%2 === 0){
     return true
@@ -22,3 +25,10 @@ function testEven(n){
     return n % 2 === 0 ? true : false;
  }
   ////////////////////////////////////////////////////////
+// personalized message (Create a function that gives a personalized greeting. This function takes two parameters: name and owner.)
+
+  function greet (name, owner) {
+  
+  return name === owner ? "Hello boss" : "Hello guest"
+  }
+    ////////////////////////////////////////////////////////
