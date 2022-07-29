948c43248e215c00ed3c9793332cdeab66b2bfb5
justinrasler <85715506+justinrasler@users.noreply.github.com>
2022-07-29 13:57:07 -0400

    volume of a cuboid

--- practice.js
+++ practice.js
@@ -46,3 +46,10 @@ function testEven(n){
         return name[0].toUpperCase() + "." + name[name.indexOf(" ")+1].toUpperCase();
     }
       ////////////////////////////////////////////////////////
+//Volume of a Cuboid(Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid. Write a function to help Bob with this calculation.)
+      class Kata {
+        static getVolumeOfCuboid(length, width, height) {
+          // your code here
+          return length * width * height
+        }
+      }
