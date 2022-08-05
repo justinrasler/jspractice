bca37d63c63e58dfb91e10917195dea6896e92e4
justinrasler <85715506+justinrasler@users.noreply.github.com>
2022-08-05 13:44:49 -0400

    commit

--- pythyon/practice.py
+++ pythyon/practice.py
@@ -6,7 +6,23 @@ print(MY_CONSTANT)
 print(my_variable)
 
 # Input function asks the questions and returns input
-user_input = input("Who are You?")
+#user_input = input("Who are You?")
 
 # We can interpolate the variable into this print
-print(f"Hey there {user_input}")
+#print(f"Hey there {user_input}")
+
+#loops
+#while loop
+counter = 0
+while (counter < 10):
+    print(counter)
+    counter += 1
+    
+
+    #for loop
+    fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+  if x == "banana":
+    break
+
