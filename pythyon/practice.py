2f1c7b0e7e3abfea0100ee201202047441b7169f
justinrasler <85715506+justinrasler@users.noreply.github.com>
2022-08-05 13:38:30 -0400

    /Library/Developer/CommandLineTools/usr/bin/python3 /Users/justin/mydevfolder/jspractice/pythyon/practice.py

--- pythyon/practice.py
+++ pythyon/practice.py
@@ -1,5 +1,12 @@
+#printing in python
 MY_CONSTANT = 5
 my_variable = "Hello World"
 
 print(MY_CONSTANT)
 print(my_variable)
+
+# Input function asks the questions and returns input
+user_input = input("Who are You?")
+
+# We can interpolate the variable into this print
+print(f"Hey there {user_input}")
