47ae7c41269698d56ee99a0fae633b9fe8a495f5
justinrasler <85715506+justinrasler@users.noreply.github.com>
2022-08-05 11:30:08 -0400

    set alarm

--- practice.js
+++ practice.js
@@ -16,6 +16,21 @@ Complete the function to return his total number of goals in all three leagues.)
     // code goes here
     return laLigaGoals + copaDelReyGoals + championsLeagueGoals
   }
+////////////////////////////////////////////////////////
+  //Set Alarm
+  /* Write a function named setAlarm which receives two parameters. The first parameter, employed, is true whenever you are employed and the second parameter, vacation is true whenever you are on vacation.
+The function should return true if you are employed and not on vacation (because these are the circumstances under which you need to set an alarm). It should return false otherwise.*/
+function setAlarm(employed, vacation){
+  if(employed === true && vacation === false){
+    return true;
+  } else{
+    return false;
+  }
+  }
+  //other answers
+  function setAlarm(employed, vacation){
+    return employed && !vacation;
+  }
    ////////////////////////////////////////////////////////
      ////////////////////////////////////////////////////////
 //7 kyu problems
