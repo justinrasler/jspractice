1ed5e43b131d49aefe047ae4c6c0eff545526257
justinrasler <85715506+justinrasler@users.noreply.github.com>
2022-12-07 15:32:40 -0500

    doublylinkedlist new code

--- jspractice/doublylinkedlist.js
+++ jspractice/doublylinkedlist.js
@@ -19,7 +19,50 @@ class DoublyLinkedList {
     }
   }
   
-  // Create your .addToTail() method below:
+  addToTail(data) {
+    const newTail = new Node(data);
+    const currentTail = this.tail;
+    if (currentTail) {
+      currentTail.setNextNode(newTail);
+      newTail.setPreviousNode(currentTail);
+    }
+    this.tail = newTail;
+    if (!this.head) {
+      this.head = newTail;
+    }
+  }
+  
+  removeHead() {
+    const removedHead = this.head;
+    if (!removedHead) {
+      return;
+    }
+    this.head = removedHead.getNextNode();
+    if (this.head) {
+      this.head.setPreviousNode(null);
+    }
+    if (removedHead === this.tail) {
+      this.removeTail();
+    }
+    return removedHead.data;
+  }
+  
+  removeTail() {
+    const removedTail = this.tail;
+    if (!removedTail) {
+      return;
+    }
+    this.tail = removedTail.getPreviousNode();
+    if (this.tail) {
+      this.tail.setNextNode(null);
+    }
+    if (removedTail === this.head) {
+      this.removeHead();
+    }
+    return removedTail.data;
+  }
+  
+  // Create your .removeByData() method below:
   
   
   printList() {
